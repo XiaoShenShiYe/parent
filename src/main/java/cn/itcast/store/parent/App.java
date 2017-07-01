01ad3dcc0ddc85da2d5cde4fef34b834025f5c81
XiaoShenShiYe <shenhongwei95@163.com>
2017-07-01 16:57:36 +0800

    build

---
+++ src/main/java/cn/itcast/store/parent/App.java
@@ -0,0 +1,13 @@
+package cn.itcast.store.parent;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ store-core/src/main/java/cn/itcast/core/web/Constants.java
@@ -0,0 +1,11 @@
+package cn.itcast.core.web;
+/**
+ * 系统常量 
+ * @author lx
+ *
+ */
+public interface Constants {
+
+	//自定义JESSIONID名称
+	public static final String CJSESSIONID = "CJSESSIONID";
+}
